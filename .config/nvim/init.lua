c7011d0b90dfa83c921f372013f1c582efb1ea4d
lvzhaoyang <lvzhaoyang@bytedance.com>
2025-09-30 13:09:54 +0800

    Add lazyvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,3 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.g.tmux_navigator_no_mappings = 1
---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- local M = {}
+--
+-- M.general = {
+--
+--   n = {
+--     ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+--   },
+-- }
+
+vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
+vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = true })
+vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
